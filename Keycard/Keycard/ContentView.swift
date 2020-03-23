04879465cb4a022d7fd948284ae0905b289078f8
rbt200 <myesoap@mail.ru>
2020-03-23 13:19:31 -0700

    Initial commit

---
+++ Keycard/Keycard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Keycard
+//
+//  Created by Ivan Ivanov on 23.03.2020.
+//  Copyright © 2020 Ivan Ivanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
